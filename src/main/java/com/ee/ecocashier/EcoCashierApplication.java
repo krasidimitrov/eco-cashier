17d555a76be3f69401f1b5db37ab8820afc3214d
Krasimir Dimitrov <kpackapgo@gmail.com>
2025-10-18 19:42:02 +0300

    Project with basic dependencies initialized. Close #1

---
+++ src/main/java/com/ee/ecocashier/EcoCashierApplication.java
@@ -0,0 +1,13 @@
+package com.ee.ecocashier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoCashierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoCashierApplication.class, args);
+	}
+
+}
