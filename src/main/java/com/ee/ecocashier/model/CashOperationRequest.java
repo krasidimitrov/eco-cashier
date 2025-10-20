3e48826e4d27c4c68fd1a49515b84ad8aaae4614
Krasimir Dimitrov <kpackapgo@gmail.com>
2025-10-21 00:55:43 +0300

    Added request object for cash operations

---
+++ src/main/java/com/ee/ecocashier/model/CashOperationRequest.java
@@ -0,0 +1,23 @@
+package com.ee.ecocashier.model;
+
+import com.ee.ecocashier.enums.CurrencyType;
+import com.ee.ecocashier.enums.OperationType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+public record CashOperationRequest (
+        @NotBlank String cashierId,
+
+        @NotNull OperationType operationType,
+
+        @NotNull CurrencyType currency,
+
+        @NotNull @Positive BigDecimal amount,
+
+        @NotEmpty Map<Integer, Integer> denominations
+) {}
