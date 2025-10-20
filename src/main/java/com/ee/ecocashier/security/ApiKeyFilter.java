066841ee6048171278c59f06d219acfe10d4caf8
Krasimir Dimitrov <kpackapgo@gmail.com>
2025-10-21 00:58:03 +0300

    Fix for warning in the filter config method.

--- src/main/java/com/ee/ecocashier/security/ApiKeyFilter.java
+++ src/main/java/com/ee/ecocashier/security/ApiKeyFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -23,8 +24,8 @@ public class ApiKeyFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain)
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
             throws ServletException, IOException {
 
         String header = request.getHeader("FIB-X-AUTH");
