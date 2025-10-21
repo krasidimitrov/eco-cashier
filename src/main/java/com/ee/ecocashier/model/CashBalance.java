1e66bf6512b60e1591d8483210ace9bf8d455e93
Krasimir Dimitrov <kpackapgo@gmail.com>
2025-10-21 05:56:45 +0300

    revert

--- src/main/java/com/ee/ecocashier/model/CashBalance.java
+++ src/main/java/com/ee/ecocashier/model/CashBalance.java
@@ -1,12 +1,10 @@
 package com.ee.ecocashier.model;
 
-import com.ee.ecocashier.enums.CurrencyType;
-
 import java.math.BigDecimal;
 import java.util.Map;
 
 public record CashBalance(
-        CurrencyType currency,
+        String currency,
         BigDecimal total,
         Map<Integer, Integer> denominations
 ) {}
